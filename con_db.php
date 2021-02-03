66ddb75b2197f318b9bc4de44d81b5add385961a
Juan Camilo <juancami98.11@hotmail.com>
2021-02-02 20:48:19 -0500

    Seguridad DB

--- con_db.php
+++ con_db.php
@@ -4,7 +4,7 @@ $host="localhost";
 $port=3306;
 $socket="";
 $user="root";
-$password= PASSWORD_MYSQLENV;  // La contraseña esta en una variable de ambiente por temas de seguridad
+$password= $PASSWORD_MYSQLENV;  // La contraseña esta en una variable de ambiente por temas de seguridad
 $dbname="registro";
 
 $con = new mysqli($host, $user, $password, $dbname, $port, $socket)
